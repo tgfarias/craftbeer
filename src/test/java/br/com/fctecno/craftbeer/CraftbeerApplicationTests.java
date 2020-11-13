3820de9c144972a49f190da00b0f0570abbe64f0
Tiago Farias <tiago.costa@ati.to.gov.br>
2020-11-13 11:12:42 -0300

    testes post

--- src/test/java/br/com/fctecno/craftbeer/CraftbeerApplicationTests.java
+++ src/test/java/br/com/fctecno/craftbeer/CraftbeerApplicationTests.java
@@ -3,6 +3,7 @@ package br.com.fctecno.craftbeer;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
+import java.math.BigDecimal;
 import java.util.Random;
 
 import org.junit.Before;
@@ -18,7 +19,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import br.com.fctecno.craftbeer.api.controller.BeerController;
+import br.com.fctecno.craftbeer.domain.model.Beer;
 import br.com.fctecno.craftbeer.domain.repository.BeerRepository;
 
 
@@ -41,7 +45,6 @@ class CraftbeerApplicationTests {
 		this.mockMvc = MockMvcBuilders.standaloneSetup(beerController).build();
 	}
 	
-	
 	@Test
 	public void testGETBeerController() throws Exception {
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/beers"))
@@ -50,11 +53,51 @@ class CraftbeerApplicationTests {
 	}
 	
 	@Test
-	public void testExistsBeer() {
-		
-		long generatedLong = new Random().nextLong();
-		assertThat(repo.existsById(generatedLong)).isFalse();
-		
+	public void testGetByIdBeerController() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/beers/{id}", 2))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
+	}
+	
+	@Test
+	public void testGetByIdNotFoundBeerController() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/beers/{id}", 1))
+		.andExpect(MockMvcResultMatchers.status().isNotFound())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
+	}
+	
+	@Test
+	public void testPostBeerController() throws Exception {
+		Beer b = new Beer();
+		b.setName("Heineken");
+		b.setCategory("Premium");
+		b.setIngredients("Água, lúpulo e malte");
+		b.setPrice(new BigDecimal(15.00));
+		b.setAlcoholContent("5.0% vol.");
+
+        mockMvc.perform(
+        		MockMvcRequestBuilders.post("/beers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(b)))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+	}
+	
+	@Test
+	public void testDeleteBeerController() throws Exception {
+	    
+	    mockMvc.perform(
+	    		MockMvcRequestBuilders.delete("/users/{id}", 9))
+	            .andExpect(MockMvcResultMatchers.status().isOk());
+	    
+	}
+	
+	
+	public static String asJsonString(final Object obj) {
+	    try {
+	        return new ObjectMapper().writeValueAsString(obj);
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
 	}
 	
 }
